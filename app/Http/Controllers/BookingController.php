c43d4c6a01507d0a929d45fea3b3f7c44fada361
ByteShah <jaiminshah.ele15@gmail.com>
2024-06-29 19:38:22 +0530

    [UPDATE] booking controller

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Booking;
 use App\Models\Workspace;
+use App\Models\Customer;
 use App\Models\Conference;
 use Illuminate\Http\Request;
 
@@ -16,7 +17,13 @@ class BookingController extends Controller
             foreach ($booking as $key => $value) {
                 $workspace_id = isset($value->workspace_id) ? $value->workspace_id : 0;
                 $conference_id = isset($value->conference_id) ? $value->conference_id : 0;
-                $value->workspace = Workspace::where('id',$workspace_id)->first();
+                $customer_id = isset($value->customer_id) ? $value->customer_id : 0;
+                $value->workspace = $workspace = Workspace::where('id',$workspace_id)->first();
+                $value->workspace_name = isset($workspace->name) ? $workspace->name : '';
+                $users = Customer::where('id',$customer_id)->first();
+                $first_name = isset($users->first_name) ? $users->first_name : '';
+                $last_name = isset($users->last_name) ? $users->last_name : '';
+                $value->customer_name = $first_name . ' ' . $last_name;
                 $value->conference = Conference::where('workspace_id',$workspace_id)->where('id',$conference_id)->first();
             }
         }
@@ -42,7 +49,13 @@ class BookingController extends Controller
         $data = Booking::findOrFail($id);
         $workspace_id = isset($data->workspace_id) ? $data->workspace_id : 0;
         $conference_id = isset($data->conference_id) ? $data->conference_id : 0;
-        $data->workspace = Workspace::where('id',$workspace_id)->first();
+        $customer_id = isset($data->customer_id) ? $data->customer_id : 0;
+        $data->workspace = $workspace = Workspace::where('id',$workspace_id)->first();
+        $data->workspace_name = isset($workspace->name) ? $workspace->name : '';
+        $users = Customer::where('id',$customer_id)->first();
+        $first_name = isset($users->first_name) ? $users->first_name : '';
+        $last_name = isset($users->last_name) ? $users->last_name : '';
+        $data->customer_name = $first_name . ' ' . $last_name;
         $data->conference = Conference::where('workspace_id',$workspace_id)->where('id',$conference_id)->first();
         return response()->json(['msg' => '','data'=>$data, 'status' => true],200);
     }
@@ -52,11 +65,11 @@ class BookingController extends Controller
         $Booking = Booking::findOrFail($id);
 
         $request->validate([
-            'name' => 'required',
-            'location' => 'required',
-            'user_id' => 'required|integer',
-            'time_from' => 'nullable|date_format:H:i',
-            'time_to' => 'nullable|date_format:H:i',
+            'workspace_id' => 'required',
+            'state_of_booking' => 'required',
+            'payment' => 'required',
+            'start_time' => 'required',
+            'end_time' => 'required',
         ]);
 
         $Booking->update($request->all());
